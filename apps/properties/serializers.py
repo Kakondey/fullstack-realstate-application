f551b9433c42ebd6109e86e23c8deec54eac7404
unknown <kakondey701@gmail.com>
2023-05-16 20:59:11 +0530

    fix: added workflow

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -10,10 +10,10 @@ class PropertySerializer(serializers.ModelSerializer):
     country = CountryField(name_only=True)
     cover_photo = serializers.SerializerMethodField()
     profile_photo = serializers.SerializerMethodField()
-    photo1 = serializers.SerializerMethodField()
-    photo2 = serializers.SerializerMethodField()
-    photo3 = serializers.SerializerMethodField()
-    photo4 = serializers.SerializerMethodField()
+    # photo1 = serializers.SerializerMethodField()
+    # photo2 = serializers.SerializerMethodField()
+    # photo3 = serializers.SerializerMethodField()
+    # photo4 = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -33,17 +33,13 @@ class PropertySerializer(serializers.ModelSerializer):
             "price",
             "tax",
             "final_property_price",
-            "plot_area",
+            "plot_aread",
             "total_floors",
             "bedrooms",
             "bathrooms",
             "advert_type",
             "property_type",
             "cover_photo",
-            "photo1",
-            "photo2",
-            "photo3",
-            "photo4",
             "published_status",
             "views",
         ]
@@ -54,17 +50,17 @@ class PropertySerializer(serializers.ModelSerializer):
     def get_cover_photo(self, obj):
         return obj.cover_photo.url
 
-    def get_photo1(self, obj):
-        return obj.photo1.url
+    # def get_photo1(self, obj):
+    #     return obj.photo1.url
 
-    def get_photo2(self, obj):
-        return obj.photo2.url
+    # def get_photo2(self, obj):
+    #     return obj.photo2.url
 
-    def get_photo3(self, obj):
-        return obj.photo3.url
+    # def get_photo3(self, obj):
+    #     return obj.photo3.url
 
-    def get_photo4(self, obj):
-        return obj.photo4.url
+    # def get_photo4(self, obj):
+    #     return obj.photo4.url
 
     def get_profile_photo(self, obj):
         return obj.user.profile.profile_photo.url
